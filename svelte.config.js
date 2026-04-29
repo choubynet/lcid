e6a52a03f687d68de8115921a65e6e5bf9bba868
choubynet <s.choubrac@hotmail.fr>
2026-04-29 16:07:14 +0200

    change config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-static'
 
+const base = process.env.BASE_PATH || '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     paths: {
-      relative: true,
+      base,
     },
     adapter: adapter({
       fallback: 'index.html'
